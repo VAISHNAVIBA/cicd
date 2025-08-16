d625da654a9fa5a53ddf1d13a29837d9122c3a6f
VAISHNAVIBA <115608220+VAISHNAVIBA@users.noreply.github.com>
2025-08-16 14:37:58 +0530

    Create Navbar.jsx

---
+++ src/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav style={{ background: '#333', padding: '1rem', color: '#fff' }}>
+      <h2>My Navbar</h2>
+    </nav>
+  );
+}
